9cc01bb7f8f8011e69947099148c8bbc25cc6d7c
souhail <souhailkhalmadani11@gmail.com>
2024-10-09 15:12:08 +0200

    changed the directory strucutre

--- Qrcoder.py
+++
@@ -1,3 +0,0 @@
-import qrcode
-img=qrcode.make("Hello world!")
-img.save("BasicQR.png")
--- Qrcoder_svg.py
+++
@@ -1,6 +0,0 @@
-import qrcode.image.svg
-
-factory=qrcode.image.svg.SvgPathImage
-svg_img=qrcode.make("Hello world",image_factory=factory)
-svg_img.save("Basic_qr.svg")
-
